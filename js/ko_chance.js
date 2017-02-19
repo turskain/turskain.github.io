d3165d483d9180f8244aaf53323d75dcad20744e
turska <turskain@gmail.com>
2017-02-19 21:32:49 +0200

    forgot the commit before

--- js/ko_chance.js
+++ js/ko_chance.js
@@ -1,4 +1,4 @@
-﻿function getKOChanceText(damage, defender, field, hits, isBadDreams) {
+﻿function getKOChanceText(damage, attacker, defender, field, hits, isBadDreams) {
     if (isNaN(damage[0])) {
         return 'something broke; please tell Austin or Marty';
     }
